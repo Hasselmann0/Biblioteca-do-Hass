98c271b8e975e2c35cc6fd121692548586f6862d
Hasselmann0 <isaacbilac920@gmail.com>
2025-03-04 17:10:22 -0300

    added page "cadastrar" and your post method

--- Models/EmprestimosModel.cs
+++ Models/EmprestimosModel.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,11 @@ namespace BibliotecaDoHass.Models
     public class EmprestimosModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Digite o nome do recebedor!")]
         public string Recebedor { get; set; }
+        [Required(ErrorMessage = "Digite o nome do fornecedor!")]
         public string Fornecedor { get; set; }
+        [Required(ErrorMessage = "Digite o nome do Livro!")]
         public string LivroEmprestado { get; set; }
         public DateTime DataUltimaAtualizacao { get; set; } = DateTime.Now;
     }
